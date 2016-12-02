ad520d3ce7b27010e05acc5d1d173b6d398073b8
Alejandro Daza <alejandrodc@alejo-pc.home>
2016-12-02 12:44:53 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	_ "github.com/apdaza/juegosApiV2/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "postgres", "postgres://postgres:sistemasoas@127.0.0.1/juegodb?sslmode=disable")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
